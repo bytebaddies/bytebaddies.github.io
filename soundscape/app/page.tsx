97ac1a2d714f6b183f21e7fcb903c9415b787c18
kirthivel-ramesh <kirthivel.ramesh@tamu.edu>
2024-02-27 14:05:45 -0600

    initial styling

--- soundscape/app/page.tsx
+++ soundscape/app/page.tsx
@@ -2,10 +2,28 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex bg-[#2b2b2b] h-screen w-screen">
-      <div>
-        Hi There! Kirthi is the one who has to fix this hehe
-      </div>
+    <main className="relative flex flex-col justify-center bg-[#2b2b2b] h-screen w-screen items-center">
+      <Image
+        src="/soundscape-logo.png"
+        alt="Soundscape Logo"
+        width={500}
+        height={500}
+      />
+
+      <input
+        type="text"
+        placeholder="Search for songs"
+        style={{ background:"#3F3F3F" , caretColor: 'white', width: '550px', borderRadius: '20px', textIndent: '10px', marginBottom: '200px'}}
+        className="h-10 mt-4 p-2 text-white"
+      />
+
+      <Image 
+        src="/logo.png"
+        alt="Byte Baddies"
+        width={100}
+        height={100}
+        className="absolute bottom-0 mb-4"
+      />
     </main>
   );
 }
